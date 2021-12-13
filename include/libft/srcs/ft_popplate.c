1329ef74dd93e89e2f777e9390f3387ee99cdfe2
tp0ns <tpons@student.42.fr>
2021-12-13 12:00:04 +0100

    Fixes a segfault on 1 plate scenario

--- include/libft/srcs/ft_popplate.c
+++ include/libft/srcs/ft_popplate.c
@@ -6,7 +6,7 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 16:55:59 by tpons             #+#    #+#             */
-/*   Updated: 2021/12/10 23:35:17 by tpons            ###   ########.fr       */
+/*   Updated: 2021/12/13 11:57:47 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ t_stack	*ft_popplate(t_stack *stack)
 	if (!stack)
 		return (NULL);
 	to_del = stack->top;
-	to_del->down->up = stack->bot;
-	stack->bot->down = to_del->down;
-	stack->top = to_del->down;
+	if (stack->size > 1)
+	{
+		to_del->down->up = stack->bot;
+		stack->bot->down = to_del->down;
+		stack->top = to_del->down;
+	}
 	free(to_del);
 	stack->size--;
 	return (stack);
